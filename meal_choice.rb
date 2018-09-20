da92c5723b7cdcc449dca6951495cc8044be99e2
Brandon Teich <brandonteich123@gmail.com>
2018-09-20 00:24:18 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,3 +1,6 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 
+def meal_choice(arg1="Miles", arg2="Coltrane", arg3="Roach")
+  "#{arg1}, #{arg2}, #{arg3}."
+end
